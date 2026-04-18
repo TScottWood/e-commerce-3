d492bf76a5afbae798b3e6387fa357ec751cf9e0
Scott <Scott.Wood@TScottWood.com>
2026-04-17 22:35:36 -0500

    4th commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Nav from "./components/Nav";
+import Landing from './components/Landing';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Landing />
     </div>
   );
 }
