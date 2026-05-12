73d36bdc1088235a62d6441012460610b6d0cc71
Scott <Scott.Wood@TScottWood.com>
2026-05-11 22:34:26 -0500

    8th commit

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import Book from './ui/Book';
+import { books }from '../data'
 
 const Featured = () => {
+    console.log(books)
   return (
     <section id="features">
         <div className="container">
@@ -8,6 +11,12 @@ const Featured = () => {
                 <h2 className="section__title">
                     Featured <span className="purple">Books</span>
                 </h2>
+                <div className="books">
+                    <Book />
+                    <Book />
+                    <Book />
+                    <Book />
+                </div>    
             </div>
         </div>
     </section>    
