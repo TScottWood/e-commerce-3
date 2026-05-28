d15d787ca7e85b92aee8a37041c22142db25d54c
Scott <Scott.Wood@TScottWood.com>
2026-05-27 21:25:56 -0500

    17th commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <div className="App">
           <Nav />
           <Routes>
-          <Route path="/" />
+          <Route path="/" exact component={Home} />
           </Routes>
           <Home />
           <Footer />
