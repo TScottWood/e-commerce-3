f542c2c48302c3c21fe695b9cb0cfe73a732b78d
Scott <Scott.Wood@TScottWood.com>
2026-05-29 18:50:01 -0500

    18th commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import Nav from './components/Nav';
-import Footer from './components/Footer';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
 
 function App() {
   return (
     <Router>
       <div className="App">
-          <Nav />
-          <Routes>
+        <Nav />
+        <Routes>
           <Route path="/" exact component={Home} />
-          </Routes>
-          <Home />
-          <Footer />
+          <Route path="/books" component={Books} />
+        </Routes>
+        <Home />
+        <Footer />
       </div>
     </Router>
   );
--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { books } from "../data";
 
 const Books = () => {
   return (
-    <div>Books</div>
+    <div id="books__body">
+      <main id="books__main">
+        <section>
+          <div className="books__container">
+            <div className="row">
+              <div className="books__header">
+                <h2 className="section__title books__header--title">All Books</h2>
+                <select id="filter" defaultValue="DEFAULT">
+                  <option value="DEFAULT" disabled>Sort</option>
+                  <option value="LOW TO LOW">Price, Low to High</option>
+                  <option value="HIGH TO LOW">Price, High to Low</option>
+                  <option value="RATING">Rating</option>
+                </select>
+              </div>
+              <div className="books">
+                {books.map((book) => (
+                  <Book book={book} key={book.id} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>    
+    </div>
   )
 }
 
